585b927931001c02c0a6127a16327fd8e5f764e4
stebancito <192398777+stebancito@users.noreply.github.com>
2026-07-21 11:31:58 -0600

    fix: cancelacion de deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: './', 
-  build: {
-    outDir: 'dist',
-  }
+  plugins: [
+    tailwindcss(),
+  ],
 });
