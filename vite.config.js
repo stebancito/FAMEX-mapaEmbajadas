ea6d92fe9ceae6a194d729c1f1a8209c5a152358
stebancito <192398777+stebancito@users.noreply.github.com>
2026-07-21 11:04:18 -0600

    Configurar despliegue automático en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/famex-mapaembajadas/', 
+  build: {
+    outDir: 'dist',
+  },
   plugins: [
     tailwindcss(),
   ],
